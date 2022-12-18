24678dcae558a1d207d9c47cc1207401b68098b8
Jesung Park <redhorangy1@naver.com>
2022-12-18 12:45:43 +0900

    한번해봄

--- java.basic/src/com/java/basic/Variable.java
+++ java.basic/src/com/java/basic/Variable.java
@@ -20,6 +20,23 @@ public class Variable {
 		System.out.println(1/5); // (/)연산자는 몫을 구하는건디..
 		
 		
+		
+		//한번 그냥 해봄//
+		int kor = 98;
+		int math = 71;
+		int eng = 100;
+		int scien = 32;
+		int SUBNUM = 3;
+		
+		int total;
+		double avg;
+		
+		total = kor + math + eng + scien;
+		avg = total / (double) SUBNUM;
+		
+		System.out.println(avg);
+		System.out.println(1/5); // (/)연산자는 몫을 구하는건디..
+		
 
 	}
 
